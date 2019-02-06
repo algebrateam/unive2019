8e59129bc7b263f6d5662f1d53a2fd86a10484f1
pmrvic <mrvic.predrag@gmail.com>
2019-02-06 22:05:39 +0100

    dodan seeder za predmete, dodan prtedmet controller

--- routes/web.php
+++ routes/web.php
@@ -10,7 +10,7 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
-
+Route::resource('predmets','PredmetController');
 Route::get('/', function () {
     return view('welcome');
 });
